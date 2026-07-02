d13e2afa972a6295d78bc4eb983b57afebbcf081
MichaelC <michaelcastrorom30@gmail.com>
2026-07-02 11:58:03 -0600

    add changes in Alimento and Bebida

--- src/Alimento.java
+++ src/Alimento.java
@@ -1,2 +1,36 @@
-public class Alimento {
+public class Alimento extends Producto implements Vendible {
+    private boolean vegetariano;
+    private int calorias;
+
+    public Alimento(String nombre, double precio, boolean disponible, boolean vegetariano, int calorias) {
+        super(nombre, precio, disponible);
+        this.vegetariano = vegetariano;
+        this.calorias = calorias;
+    }
+
+    public boolean isVegetariano() {
+        return vegetariano;
+    }
+
+    public int getCalorias() {
+        return calorias;
+    }
+
+    @Override
+    public double calcularPrecioFinal() {
+        double precioFinal = getPrecio();
+
+        if (vegetariano) {
+            precioFinal *= 0.90;
+        }
+
+        return precioFinal;
+    }
+
+    @Override
+    public void mostrarInformacion() {
+        super.mostrarInformacion();
+        System.out.println("Vegetariano: " + vegetariano);
+        System.out.println("Calorías: " + calorias);
+    }
 }
--- src/Bebida.java
+++ src/Bebida.java
@@ -1,2 +1,45 @@
-public class Bebida {
+public class Bebida extends Producto implements Vendible {
+    private String tamanio;
+    private String temperatura;
+
+
+    public Bebida(String nombre, double precio, boolean disponible, String tamanio, String temperatura) {
+        super(nombre, precio, disponible);
+        this.tamanio = tamanio;
+        this.temperatura = temperatura;
+    }
+
+    public String getTemperatura() {
+        return temperatura;
+    }
+
+    public void setTemperatura(String temperatura) {
+        this.temperatura = temperatura;
+    }
+
+    public String getTamanio() {
+        return tamanio;
+    }
+
+    public void setTamanio(String tamanio) {
+        this.tamanio = tamanio;
+    }
+
+    @Override
+    public double calcularPrecioFinal() {
+        double precioFinal = getPrecio();
+
+        if (tamanio.equalsIgnoreCase("Large")) {
+            precioFinal += 15;
+        }
+
+        return precioFinal;
+    }
+
+    @Override
+    public void mostrarInformacion() {
+        super.mostrarInformacion();
+        System.out.println("Tamaño: " + tamanio);
+        System.out.println("Temperatura: " + temperatura);
+    }
 }
